536b8388e62484d1f0e9bae1948acd8e3927882e
BalajiViswa22 <110532122+BalajiViswa22@users.noreply.github.com>
2023-09-22 11:23:04 +0530

    Add files via upload

---
+++ day5/src/components/Leftbar.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useDispatch } from 'react-redux';
+import { logout } from '../redux/userSlice';
+
+
+function Leftbar() {
+
+    const dispatch= useDispatch();
+    const navigate=useNavigate();
+
+    const homeHandler = () =>{
+        navigate('/Home')
+     }
+
+
+const dashboardHandler = ()=>{
+    navigate('/Dashboard')
+}
+const settingsHandler = ()=>{
+    navigate('/Settings')
+}
+const usersHandler = () =>{
+    navigate('/Users')
+}
+const logoutHandler = () =>{
+    dispatch(logout())
+    navigate('/')
+}
+    return (
+        <>
+            <div className='left-bar'>
+                <div className='left-bar-container'>
+                <button className='left-bar-button nav-btn' onClick={homeHandler}>
+              
+                        Home
+                    </button><br/>
+                    <button className='left-bar-button nav-btn' onClick={dashboardHandler}>
+                        Dashboard
+                    </button><br/>
+                    <button className='left-bar-button nav-btn' onClick={usersHandler}>
+                        Instructional Videos
+                    </button><br/>
+                    <button className='left-bar-button nav-btn' onClick={settingsHandler}>
+                        Workout Plan
+                    </button>
+                    <button className='left-bar-button-logout nav-btn' onClick={logoutHandler}>
+                        Logout
+                    </button>
+
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Leftbar
